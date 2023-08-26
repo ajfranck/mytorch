c81995651de276a7752f3115685c20f827f5fe05
Andy <1amajfranck@gmail.com>
2023-08-25 21:24:23 -0700

    removed redundant files

--- MyTorch for NN/HW1/mytorch/nn/__init__.py
+++
@@ -1,3 +0,0 @@
-from .activation import Identity, Sigmoid, Tanh, ReLU
-from .linear import Linear
-from .loss import MSELoss, CrossEntropyLoss
--- MyTorch for RNN/HW2/mytorch/nn/__init__.py
+++
@@ -1,4 +0,0 @@
-from .activation import Identity, Sigmoid, Tanh, ReLU
-from .linear import Linear
-from .loss import MSELoss, CrossEntropyLoss
-from .conv import Conv2d
